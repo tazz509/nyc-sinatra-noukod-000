b9289f125276391c8dc9ea7a0d43aa81cbba62c2
carly <carly@noukod.com>
2019-01-18 17:25:16 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,39 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get "/landmarks/new" do
+    @titles=Title.all
+    @landmarks=Landmark.all
+    erb :"landmarks/new"
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark=Landmark.find_by_id(@params[:id])
+    erb :"landmarks/edit"
+  end
+
+  get "/landmarks/:id" do
+    @landmark=Landmark.find_by_id(@params[:id])
+
+    erb :"landmarks/show"
+  end
+
+  get "/landmarks" do
+    @landmarks=Landmark.all
+    erb :"landmarks/index"
+  end
+
+  post "/landmarks" do
+    @landmark=Landmark.create(@params[:landmark])
+
+    redirect "/landmarks/#{@landmark.id}"
+
+  end
+
+  patch "/landmarks/:id" do
+    @landmark=Landmark.find_by_id(@params[:id])
+    @landmark.name=@params[:landmark][:name]
+    @landmark.year_completed=@params[:landmark][:year_completed]
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
 end
--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,3 +1,5 @@
 class Landmark < ActiveRecord::Base
   # add relationships here
+  belongs_to :title
+  belongs_to :figure
 end
